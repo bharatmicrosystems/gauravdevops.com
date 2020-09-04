504418ca426caf75af4fff19a4c9e497d6019f2e
Gaurav Agarwal <gaurav.a.agarwal@capgemini.com>
2020-09-04 01:16:41 +0100

    Updated blog page to include description

--- assets/js/generate_blogs.py
+++ assets/js/generate_blogs.py
@@ -6,13 +6,13 @@ import time
 import pandas as pd
 
 # Read the csv and assign to a schedule dataframe
-posts = pd.read_csv('medium.csv')
+posts = pd.read_csv('medium.csv', encoding = "UTF-8")
 
 # Iterate over the dataframe rows and schedule jobs initially
 job_id = 1
 list_of_posts = []
 for row in posts.itertuples(index=True, name='Pandas'):
-    post = {"title": row.title, "subtitle": row.subtitle,
+    post = {"title": row.title, "subtitle": row.subtitle, "description": row.description,
             "image_id": row.image_id, "link": row.link, "published": row.published, "read": row.read, "published_on": row.published_on}
     list_of_posts.append(post)
 

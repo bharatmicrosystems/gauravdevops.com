928dd1a6b83b6fab7c9b80c2c9ebdfcadbbd3f6b
Gaurav Agarwal <gaurav.a.agarwal@capgemini.com>
2020-09-09 00:12:32 +0100

    Added template index

--- assets/js/generate_blogs.py
+++ assets/js/generate_blogs.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from threading import Thread
 import time
 import pandas as pd
+import urllib.parse
 
 # Read the csv and assign to a schedule dataframe
 posts = pd.read_csv('medium.csv', encoding="UTF-8")
@@ -13,7 +14,9 @@ job_id = 1
 list_of_posts = []
 txt = ""
 for row in posts.itertuples(index=True, name='Pandas'):
-    txt = txt + "<div class=\"item mb-5\">  <div class=\"media\">      <div class=\"media-body\">      <h3 class=\"title mb-1\"><a href=\"" + row.link + "\" target=\"_blank\">" + row.title + "</a></h3> <div class=\"meta mb-1\"><span class=\"date\">Published " + row.published + "</span><span class=\"time\">" + row.read + "</span></div>           <div class=\"intro\"><p>" + row.subtitle + "</p><p><img class=\"mr-3 img-fluid d-none d-md-flex\" src=\"https://miro.medium.com/max/875/" + row.image_id + "\" alt=\"image\"></p>" + row.description + "</div>      <a class=\"more-link\" href=\"" + row.link + "\" target=\"_blank\">Read more &rarr;</a>    </div><!--//media-body-->  </div><!--//media--></div><!--//item-->"
+    link = urllib.parse.quote_plus(row.link.replace("https://medium.com/",""))
+    link = "blog.html?slug=" + link
+    txt = txt + "<div class=\"item mb-5\">  <div class=\"media\">      <div class=\"media-body\">      <h3 class=\"title mb-1\"><a href=\"" + link + "\" target=\"_blank\">" + row.title + "</a></h3> <div class=\"meta mb-1\"><span class=\"date\">Published " + row.published + "</span><span class=\"time\">" + row.read + "</span></div>           <div class=\"intro\"><p>" + row.subtitle + "</p><p><img class=\"mr-3 img-fluid d-none d-md-flex\" src=\"https://miro.medium.com/max/875/" + row.image_id + "\" alt=\"image\"></p>" + row.description + "</div>      <a class=\"more-link\" href=\"" + row.link + "\" target=\"_blank\">Read more &rarr;</a>    </div><!--//media-body-->  </div><!--//media--></div><!--//item-->"
 
 with open('index.template', 'r') as file:
     data = file.read().replace('<div class="container" id="blog_container">', '<div class="container" id="blog_container">'+txt)
